39300055e53137957a90f56d92dad0b857c48ead
jrmeurer <jrmeurer@wisc.edu>
2015-05-22 15:10:59 -0500

    testing and merging to master

--- www/js/controllers/profileController.js
+++ www/js/controllers/profileController.js
@@ -488,33 +488,37 @@ paradropCtrl.controller('profileCtrl',
 
 		 $scope.data = {notificationFrequency: "day"}
 		 //alert($scope.data.notificationFrequency)
-		$scope.showAlarmView = function() {
-			var alertPopup = $ionicPopup.show({
-        scope: $scope,
-        title: 'Reminder',
-        templateUrl: 'templates/alarm.html',
-        buttons: [{ text: 'Cancel' }, 
-        {text: 'Save', 
-        type: 'button-balanced',
-        onTap: function(e) {
-        	//Cancel Previous Notification
-        	window.plugin.notification.local.cancel(1, function() {});
 
-        	
-        	var desiredDate = moment();
-        	//desiredDate.add(15,"seconds")
-        	desiredDate.add(1,"day")
-        	desiredDate.set({'hour': 15, 'minute': 00});
-        	//alert(desiredDate.toDate())
-          window.plugin.notification.local.schedule({
-                     id: 1,
-                     text: 'Remember to complete your carbon Activities!',
-                     every: $scope.data.notificationFrequency,
-                     firstAt: desiredDate.toDate()
-          });
-        }
-      }]
-    });
+
+		$scope.showAlarmView = function() {
+
+			var alertPopup = $ionicPopup.show({
+		        scope: $scope,
+		        title: 'Reminder',
+		        templateUrl: 'templates/alarm.html',
+		        buttons: [{ text: 'Cancel' }, 
+		        {text: 'Save', 
+		        type: 'button-balanced',
+		        onTap: function(e) {
+		        	//Cancel Previous Notification
+		        	window.plugin.notification.local.cancel(1, function() {});
+		        	
+		        	var desiredDate = moment();
+		        	desiredDate.add(15,"seconds")
+		        	// desiredDate.add(1,"day")
+		        	// desiredDate.set({'hour': 15, 'minute': 00});
+		        	//alert(desiredDate.toDate())
+		          	window.plugin.notification.local.schedule({
+	                     id: 1,
+	                     text: 'Remember to complete your carbon Activities!',
+	                     every: $scope.data.notificationFrequency,
+	                     firstAt: desiredDate.toDate()
+		          	});
+
+		          	alertPopup.hide();
+		        }
+		      }]
+		    });
 		}
 
 
